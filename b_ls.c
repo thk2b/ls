967a705ac1a9ec39fb05cc843b16a1fb82699ff0
thk2b <theokobb@gmail.com>
2018-09-17 19:37:04 -0700

    R

--- b_ls.c
+++ b_ls.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/17 09:52:33 by tkobb             #+#    #+#             */
-/*   Updated: 2018/09/17 17:26:44 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/09/17 18:16:44 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #include <dirent.h>
 #include <limits.h>
 #include <sys/stat.h>
-
+#include <stdio.h>
 static void	(*g_traverse) (t_btree *root, void *ctx, void(*f)(void *ctx, void *data));
 
 static char	*path_join(char *dst, const char *base, const char *file)
@@ -34,12 +34,13 @@ static void	b_ls_dir(void *ctx, void *data)
 	struct s_opts	*opts;
 	struct s_file	*dir_data;
 	t_btree			*tree;
+	t_btree			*recursive_tree;
 	DIR				*dir;
 	struct dirent	*child;
 	struct s_file	*child_data;
 	char			path[PATH_MAX];
-
 	tree = NULL;
+	recursive_tree = NULL;
 	opts = (struct s_opts*)ctx;
 	dir_data = (struct s_file*)data;
 	if ((dir = opendir(dir_data->path)) == NULL)
@@ -55,13 +56,22 @@ static void	b_ls_dir(void *ctx, void *data)
 		if (opts->all == 0 && child->d_name[0] == '.')
 			continue ;
 		if((child_data = get_file(opts, child->d_name, path_join(path, dir_data->path, child->d_name))) == NULL)
+		{
 			(void)error(path);
+			continue ;
+		}
 		// recursive: if child is a dir, add it to the dir tree and add it to the tree
-		else
-			btree_add(&tree, (void*)child_data, (void*)opts, cmp_files);
+		if (opts->recursive && child_data->is_dir)
+			btree_add(&recursive_tree, (void*)child_data, (void*)opts, cmp_files);
+		btree_add(&tree, (void*)child_data, (void*)opts, cmp_files);
 	}
 	g_traverse(tree, (void*)opts, print_file);
 	btree_free(tree);
+	if (recursive_tree)
+	{
+		g_traverse(recursive_tree, (void*)opts, b_ls_dir);
+		btree_free(recursive_tree);
+	}
 }
 
 int			b_ls(struct s_opts *opts, const char **filenames)
@@ -92,12 +102,12 @@ int			b_ls(struct s_opts *opts, const char **filenames)
 	}
 	if (filetree != NULL)
 	{
-		g_traverse(filetree, opts, print_file);
+		g_traverse(filetree, (void*)opts, print_file);
 		btree_free(filetree);
 	}
 	if (dirtree != NULL)
 	{
-		g_traverse(dirtree, opts, b_ls_dir);
+		g_traverse(dirtree, (void*)opts, b_ls_dir);
 		btree_free(dirtree);
 	}
 	return (0);
