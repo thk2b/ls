ad24245cc2fe3e14b0a636d2fb3c074dd3a8f84a
thk2b <theokobb@gmail.com>
2018-11-10 00:56:10 -0800

    split render utils

--- srcs/render_utils.c
+++ srcs/render_utils.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/25 18:26:48 by tkobb             #+#    #+#             */
-/*   Updated: 2018/11/10 00:27:32 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/11/10 00:53:38 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,40 +92,4 @@ char		*group(gid_t i)
 		return (NULL);
 	}
 	return (g->gr_name);
-}
-
-char		*get_time(time_t *t)
-{
-	char *s;
-
-	s = ctime(t);
-	s[ft_strlen(s) - 1] = '\0';
-	return (s);
-}
-
-char		*get_link(t_file *file, struct stat *st)
-{
-	char	*s;
-	char	name[PATH_MAX];
-
-	ft_bzero(name, PATH_MAX);
-	s = NULL;
-	if (!S_ISLNK(st->st_mode))
-		return (ft_strdup(""));
-	if (readlink(file->path, (char*)&name, PATH_MAX) == -1)
-	{
-		error(file->path);
-		return (NULL);
-	}
-	MCK(ft_asprintf(&s, " -> %s", name), NULL);
-	return (s);
-}
-
-char		*maj_min(dev_t dev)
-{
-	char	*s;
-
-	s = NULL;
-	ft_asprintf(&s, "%3d, %3d", major(dev), minor(dev));
-return (s);
 }
