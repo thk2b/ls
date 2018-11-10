58b5e49b26e55b4563b512eaadd20182eb834520
thk2b <theokobb@gmail.com>
2018-11-09 23:28:50 -0800

    update error message

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/23 14:07:09 by tkobb             #+#    #+#             */
-/*   Updated: 2018/11/09 20:49:09 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/11/09 23:25:56 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 int		error(char *filename)
 {
 	if (filename)
-		ft_printf("%s: %s: %s\n", "b_ls", filename, strerror(errno));
+		ft_printf("%s: %s: %s\n", "ft_ls", filename, strerror(errno));
 	else
-		ft_printf("%s: %s\n", "b_ls", strerror(errno));
+		ft_printf("%s: %s\n", "ft_ls", strerror(errno));
 	return (errno);
 }
